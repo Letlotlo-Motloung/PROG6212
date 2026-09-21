af72f69240b01ae00c256db520747a1edb576f38
st10365209 <126856472+Letlotlo-Motloung@users.noreply.github.com>
2026-09-21 18:39:46 +0200

    Add FK-safe drop table statements to schema
    Added drop table statements for FK-safe order.

--- docs/Raceday_schema.sql
+++ docs/Raceday_schema.sql
@@ -11,7 +11,18 @@ GO
  
 USE RaceDay;
 GO
- 
+
+/*
+   Drop tables if they already exist, in FK-safe order, so the
+   script can be re-run cleanly on the same instance. 
+*/
+IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
+IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
+IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
+IF OBJECT_ID('dbo.Roles', 'U') IS NOT NULL DROP TABLE dbo.Roles;
+GO
 
 /* 
    TABLE: Roles
